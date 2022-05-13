57ce59a41fd3af0499e0298e50fb6c510646098e
kunle5 <66616411+kunle5@users.noreply.github.com>
2022-05-13 18:55:59 +0100

    Update main.h

--- main.h
+++ main.h
@@ -145,14 +145,14 @@ _Bool splitString(config *build);
 unsigned int countWords(char *s);
 _Bool isSpace(char c);
 
-/* string_helpers1 */
+/* string_helpers */
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strdup(char *str);
 char *_strcpy(char *dest, char *src);
 
-/* string_helpers2 */
+/* string_helpers1 */
 char *_strtok(char *str, char *delim);
 int _strcspn(char *string, char *chars);
 char *_strchr(char *s, char c);
